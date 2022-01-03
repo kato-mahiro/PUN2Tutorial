3b91ac8262b6910612dbf90ad4ed9a31f77bbae1
katomasahiro10 <katomasahiro10@gmail.com>
2022-01-03 12:23:58 +0900

    Chap.4 まで

--- Assets/Scripts/AvatarController.cs
+++ Assets/Scripts/AvatarController.cs
@@ -1,10 +1,15 @@
 using System;
+using System.Collections.Concurrent;
 using Photon.Pun;
 using UnityEngine;
+using UnityEngine.UI;
 
 // MonoBehaviourPunCallbacksを継承して、photonViewプロパティを使えるようにする
-public class AvatarController : MonoBehaviourPunCallbacks
+public class AvatarController : MonoBehaviourPunCallbacks, IPunObservable
 {
+    private const float MaxStamina = 1f;
+    private float currentStamina = MaxStamina;
+    public Slider staminaBar;
     private void Start()
     {
         PhotonNetwork.NickName = "Player";
@@ -14,7 +19,30 @@ public class AvatarController : MonoBehaviourPunCallbacks
         // 自身が生成したオブジェクトだけに移動処理を行う
         if (photonView.IsMine) {
             var input = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f);
-            transform.Translate(6f * Time.deltaTime * input.normalized);
+            if (input.sqrMagnitude > 0f)
+            {
+                currentStamina = Mathf.Max(0f, currentStamina - Time.deltaTime);
+                transform.Translate(6f * Time.deltaTime * input.normalized);
+            }
+            else
+            {
+                currentStamina = Mathf.Min(currentStamina + Time.deltaTime * 2, MaxStamina);
+            }
+        }
+
+        staminaBar.value = currentStamina / MaxStamina;
+    }
+
+    void IPunObservable.OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            //自身のアバターのスタミナを送信する
+            stream.SendNext(currentStamina);
+        }
+        else
+        {
+            currentStamina = (float) stream.ReceiveNext();
         }
     }
 }
