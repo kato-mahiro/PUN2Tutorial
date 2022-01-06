a8f88deca8bd06f80517fe27ba7ca7fff53071fa
katomasahiro10 <katomasahiro10@gmail.com>
2022-01-06 18:08:30 +0900

    Chap.6まで(カスタムプロパティ)

--- Assets/Scripts/AvatarFireBullet.cs
+++ Assets/Scripts/AvatarFireBullet.cs
@@ -6,6 +6,8 @@ public class AvatarFireBullet : MonoBehaviourPunCallbacks
     [SerializeField]
     private Bullet bulletPrefab = default;
 
+    private int nextBulletId = 0;
+
     private void Update() {
         if (photonView.IsMine) {
             // 左クリックでカーソルの方向に弾を発射する
@@ -14,15 +16,15 @@ public class AvatarFireBullet : MonoBehaviourPunCallbacks
                 var direction = mousePosition - transform.position;
                 float angle = Mathf.Atan2(direction.y, direction.x);
 
-                photonView.RPC(nameof(FireBullet), RpcTarget.All, angle);
+                photonView.RPC(nameof(FireBullet), RpcTarget.All, nextBulletId++, angle);
             }
         }
     }
 
     // 弾を発射するメソッド
     [PunRPC]
-    private void FireBullet(float angle) {
+    private void FireBullet(int id, float angle) {
         var bullet = Instantiate(bulletPrefab);
-        bullet.Init(transform.position, angle);
+        bullet.Init(id, photonView.OwnerActorNr, transform.position, angle);
     }
 }
--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -3,11 +3,22 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     private Vector3 _velocity;
+    
+    //弾のIDを返すプロパティ
+    public int Id { get; private set; }
+    //弾を発射したプレイヤーのIDを返すプロパティ
+    public int OwnerId { get; private set; }
+    //同じ弾かどうかをIDで判定するメソッド
+    public bool Equals(int id, int ownerId) => id == Id && ownerId == OwnerId;
 
-    public void Init(Vector3 origin, float angle) {
+    public void Init(int id, int ownerId, Vector3 origin, float angle)
+    {
+        Id = id;
+        OwnerId = ownerId;
         transform.position = origin;
         _velocity = 9f * new Vector3(Mathf.Cos(angle), Mathf.Sin(angle));
     }
+
 
     private void Update() {
         transform.Translate(_velocity * Time.deltaTime);
